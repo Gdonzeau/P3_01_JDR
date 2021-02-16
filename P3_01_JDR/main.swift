23048df4ae797143c44fd4ae38363c9b18754272
GuillaumeDonzeau <gdonzeau@hotmail.com>
2021-02-16 23:32:53 +0100

    Update main.swift
    Vérification du nom à retravailler pour l'adapter aux héros également

--- P3_01_JDR/main.swift
+++ P3_01_JDR/main.swift
@@ -8,13 +8,54 @@
 import Foundation
 
 class Game {
-    var state = gameState.going
-    
     enum gameState {
         case over
         case going
     }
+    var state:gameState = .going
+    var Players = [player1,player2]
+    var nameAlreadyExists = false
+    
+    func giveNameToPlayer() {
+        for i in 0..<Players.count {
+            print("Bonjour Joueur \(i+1). Comment vous appelez-vous ?")
+            if let text = readLine() {
+                var names = [player1.name,player2.name]
+                if checkName(nameToCheck: text,names:names ) {
+                    giveNameToPlayer()
+                }
+                
+                else {
+                   // let newName =
+                names[i] = text
+                    if i == 0 {
+                        player1.name = text
+                    }
+                    if i == 1 {
+                        player2.name = text
+                    }
+                    //Players[i].name = names[i]
+                print("Bonjour \(names[i]).")
+                }
+            }
+        }
+    }
+    
+    func checkName(nameToCheck: String,names:[String]) -> Bool {
+        for name in names {
+            if nameToCheck == name {
+                print("Ce nom existe déjà.")
+                nameAlreadyExists = true
+                break
+            }
+            else {
+                nameAlreadyExists = false
+            }
+        }
+        return nameAlreadyExists
+    }
 }
+
 var game = Game()
 
 class Player {
@@ -23,9 +64,15 @@ class Player {
 }
 
 var player1 = Player()
+var player2 = Player()
 
-class Hero:Player {
-    
+class Hero {
+    enum heroClass {
+        case Barbarian, Ranger, Mage, Warrior, Druid, toDefine
+    }
+    var name = ""
+    var HP = 100
+    var type:heroClass = .toDefine
 }
 
 class Stuff {
@@ -43,5 +90,6 @@ class Weapon:Stuff {
 class Heal:Stuff {
     
 }
+var premierHero = Hero()
 
-
+game.giveNameToPlayer()
